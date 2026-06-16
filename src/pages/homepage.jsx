67b60fa16535116d35e1fc94d675db51df5624cb
Prayushstha <prayushstha1212@gmail.com>
2026-06-16 17:12:42 +0545

    Initial Commit: Start of Ecommers Project

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,10 @@
+import { NavBar } from "../components/navbar.jsx";
+import { ItemsContainerHomePage } from "../components/items-container.jsx";
+export function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <ItemsContainerHomePage />
+    </>
+  );
+}
