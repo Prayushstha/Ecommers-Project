bec03fe0b9f45476ffe01b81dd01093f4371f2c9
Prayushstha <prayushstha1212@gmail.com>
2026-06-17 10:45:34 +0545

    Finished course 7:  React with Backend, Data Fetching, Load Ecommerce Project Data

--- src/pages/tracking.jsx
+++ src/pages/tracking.jsx
@@ -6,53 +6,52 @@ import dayjs from "dayjs";
 import { useParams } from "react-router";
 import axios from "axios";
 
-export function TrackingPage({ orders }) {
-  const {orderId,productId} = useParams();
-  const [trackingOrder,setTrackingOrder] = useState(null);
-  
-  useEffect(()=>{
-    axios.get(`http://localhost:3000/api/orders/${orderId}?expand=products`).then((response)=>{
+export function TrackingPage() {
+  const { orderId, productId } = useParams();
+  const [trackingOrder, setTrackingOrder] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/api/orders/${orderId}?expand=products`).then((response) => {
       setTrackingOrder(response.data);
-      console.log(response.data)
     })
-  },[orderId])
-  if(!trackingOrder) return null;
+  }, [orderId]);
+
+  if (!trackingOrder) return null;
+
+  const orderProduct = trackingOrder.products.find(p => p.productId === productId);
+
+  if (!orderProduct) return null;
+
+
+  const totalDeliveryTimeMs = orderProduct.estimatedDeliveryTimeMs - trackingOrder.orderTimeMs;
+  const timePassedMs = dayjs().valueOf() - trackingOrder.orderTimeMs;
+  const deliveryPercent = Math.min((timePassedMs / totalDeliveryTimeMs) * 100, 100);
+
   return (
     <>
       <title>Checkout</title>
-    <NavBar/>
+      <NavBar />
       <div className="tracking-page">
         <div className="order-tracking">
           <a className="back-to-orders-link link-primary" href="orders">
             View all orders
           </a>
-          {
-         
-            trackingOrder.find((item) => {
-              return (
-                <Fragment key={item.productId}>
-                  <div className="delivery-date">
-                    {dayjs(item.estimatedDeliveryTimeMs).format('MMMM D')}
-                  </div>
-                  <div className="product-info">{item.product.name}</div>
-                  <div className="product-info">Quantity: {item.quantity}</div>
-                  <img
-                    className="product-image"
-                    src={item.product.image}
-                  />
-
-                  <div className="progress-labels-container">
-                    <div className="progress-label">Preparing</div>
-                    <div className="progress-label current-status">Shipped</div>
-                    <div className="progress-label">Delivered</div>
-                  </div>
-                  <div className="progress-bar-container">
-                    <div className="progress-bar"></div>
-                  </div>
-                </Fragment>
-              )
-            )
-          }
+          <Fragment key={orderProduct.productId}>
+            <div className="delivery-date">
+              {dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D')}
+            </div>
+            <div className="product-info">{orderProduct.product.name}</div>
+            <div className="product-info">Quantity: {orderProduct.quantity}</div>
+            <img className="product-image" src={orderProduct.product.image} />
+            <div className="progress-labels-container">
+              <div className="progress-label">Preparing</div>
+              <div className="progress-label current-status">Shipped</div>
+              <div className="progress-label">Delivered</div>
+            </div>
+            <div className="progress-bar-container">
+              <div className="progress-bar" style={{ width: `${deliveryPercent}%` }}></div>
+            </div>
+          </Fragment>
         </div>
       </div>
     </>
